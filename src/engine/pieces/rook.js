a55aff6f219591bf37074c0d713e0bebfda90b09
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-11 14:53:35 +0000

    Rook can go up and down

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,46 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        return [...new Set([...this.getVerticalMoves(board, location), ...this.getHorizontalMoves(board, location)])]
+    }
+
+    getVerticalMoves(board, location)
+    {
+        let moves = [];
+        let directions = [1, -1];
+
+        for (const direction of directions)
+        {
+            let i = direction;
+
+            let potentialMove = Square.at(location.row, location.col + i);
+
+            while(board.isOnTheBoard(potentialMove) && board.isFreeOrCapturable(potentialMove)){
+                moves.push(potentialMove);
+                i += direction
+                potentialMove = Square.at(location.row, location.col + i);
+            }
+        }
+        return moves;
+    }
+
+    getHorizontalMoves(board, location)
+    {
+        let moves = [];
+        let directions = [1, -1];
+        for (const direction of directions)
+        {
+            let i = direction;
+
+            let potentialMove = Square.at(location.row + i, location.col);
+
+            while(board.isOnTheBoard(potentialMove) && board.isFreeOrCapturable(potentialMove)){
+                moves.push(potentialMove);
+                i += direction
+                potentialMove = Square.at(location.row + i, location.col);
+            }
+        }
+        return moves;
     }
 }
