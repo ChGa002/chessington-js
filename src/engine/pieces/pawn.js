86939fbdfacdf7e0b3c09e22ce558fbd65db7571
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-11 12:23:38 +0000

    Double first move for pawns

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,15 +1,25 @@
 import Piece from './piece';
 import Square from "../square";
 import Player from "../player";
+import gameSettings from "../gameSettings";
 
 export default class Pawn extends Piece {
+
     constructor(player) {
         super(player);
     }
 
     getAvailableMoves(board) {
         const location = board.findPiece(this);
-        const direction = this.player === Player.WHITE ? 1 : -1;
-        return new Array(Square.at(location.row + direction, location.col));
+        let direction = this.player === Player.WHITE ? 1 : -1;
+
+        let moves = [Square.at(location.row + direction, location.col)];
+
+        if ((this.player === Player.WHITE && location.row === 1) || (this.player === Player.BLACK && location.row === gameSettings.BOARD_SIZE - 2))
+        {
+            moves.push(Square.at(location.row + direction * 2, location.col));
+        }
+
+        return moves;
     }
 }
