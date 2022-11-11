d8e80dca552c9d27206221264f8e15f231994964
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-11 15:48:24 +0000

    Bishop can now move diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,33 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        return [...new Set([
+            ...this.getDiagonalMoves(board, location, 1, -1),
+            ...this.getDiagonalMoves(board, location, 1, 1 ),
+            ...this.getDiagonalMoves(board, location, -1, 1 ),
+            ...this.getDiagonalMoves(board, location, -1, -1 )])]
+
+    }
+
+    // x, y represent the direction
+    getDiagonalMoves(board, location, x, y)
+    {
+        let moves = [];
+
+        let i = 1;
+        let potentialMove = Square.at(location.row + x, location.col + y);
+
+        while(board.isOnTheBoard(potentialMove) && board.isFreeOrCapturable(potentialMove)){
+            moves.push(potentialMove);
+
+            if (board.isCapturable(potentialMove)) break;
+
+            i ++;
+
+            potentialMove = Square.at(location.row + i * x, location.col + i * y);
+        }
+        return moves;
+
     }
 }
